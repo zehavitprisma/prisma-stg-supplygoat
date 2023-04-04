073c2dc2ed2ca39498990f2b3b190034e71c3c3e
zehavitprisma <108680422+zehavitprisma@users.noreply.github.com>
2023-04-04 16:08:12 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,7 +58,6 @@ resource "aws_s3_bucket" "logs" {
     rule {
       apply_server_side_encryption_by_default {
         sse_algorithm     = "aws:kms"
-        kms_master_key_id = "${aws_kms_key.logs_key.arn}"
       }
     }
   }
